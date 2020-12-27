234e1b07a32b0af2c265b6531c557e578d7ad105
Philipp Hug <philipp@hug.cx>
2020-12-27 20:04:56 +0400

    tileset must be embedded

--- text_coreboot.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="text_coreboot" tilewidth="32" tileheight="32" tilecount="1089" columns="33">
- <image source="tiles_png/cy/text-coreboot.png" width="1056" height="1056"/>
-</tileset>
